3aaf2ee4ecf403faab575d3bc00f48308a3b0817
Daniel-Rufinox <danielruf1999@gmail.com>
2026-09-23 17:23:32 -0300

    Atualizar o main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
 
+from .database import Base, engine
+from . import models
+from .routes import profiles, technologies, projects
+
+
+# Cria as tabelas do banco de dados
+Base.metadata.create_all(bind=engine)
+
+
 app = FastAPI(
     title="DevShowcase API",
     description="API para gerenciamento de perfis, projetos, tecnologias e feedbacks.",
     version="1.0.0"
 )
+
+
+# Rotas da API
+app.include_router(profiles.router)
+app.include_router(technologies.router)
+app.include_router(projects.router)
 
 
 @app.get("/")
