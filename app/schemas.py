cb363ae4d6ae8540b022ab05f3e572f59788eb4b
Daniel-Rufinox <danielruf1999@gmail.com>
2026-09-24 19:12:43 -0300

    Atualizar o schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -40,6 +40,26 @@ class TechnologyResponse(BaseModel):
 
 
 # =========================
+# FEEDBACK
+# =========================
+
+class FeedbackCreate(BaseModel):
+    author: str = Field(..., min_length=2, max_length=100)
+    comment: str = Field(..., min_length=2, max_length=1000)
+    rating: int = Field(..., ge=1, le=5)
+
+
+class FeedbackResponse(BaseModel):
+    id: int
+    author: str
+    comment: str
+    rating: int
+    project_id: int
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+# =========================
 # PROJECT
 # =========================
 
@@ -60,5 +80,7 @@ class ProjectResponse(BaseModel):
     deploy_url: HttpUrl | None
     profile_id: int
     technology_ids: list[int]
+    average_rating: float
+    upvotes: int
 
     model_config = ConfigDict(from_attributes=True)
