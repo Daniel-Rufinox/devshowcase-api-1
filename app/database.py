444bc3c37a97448a4abc2a8c008bd4fa2561728b
Daniel-Rufinox <danielruf1999@gmail.com>
2026-09-24 19:44:42 -0300

    Atualizar o database.py

--- app/database.py
+++ app/database.py
@@ -1,12 +1,26 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./devshowcase.db"
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./devshowcase.db"
 )
+
+
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(
+        DATABASE_URL,
+        pool_pre_ping=True
+    )
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -19,6 +33,7 @@ Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
+
     try:
         yield db
     finally:
